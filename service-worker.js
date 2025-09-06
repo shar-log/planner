1a939b493f4c00a108a9e919f924c1d45a3fd32e
shar-log <sharmithak@gmail.com>
2025-09-06 23:43:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,33 @@
-const CACHE = 'habit-tracker-v1';
-const FILES = ['/', '/index.html', '/style.css', '/script.js', '/background.jpg', '/manifest.json'];
+const CACHE_NAME = 'habit-tracker-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/confetti.mp3',
+  '/icon192.png',
+  '/icon512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if(key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });

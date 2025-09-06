5b93a8f9dd3317c0025a6323484b4531ce071e19
shar-log <sharmithak@gmail.com>
2025-09-06 23:20:32 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,22 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCTArFuPpxo608354Ql9RLOZRB9lGHFndI",
+  authDomain: "habit-tracker-3eb0d.firebaseapp.com",
+  projectId: "habit-tracker-3eb0d",
+  storageBucket: "habit-tracker-3eb0d.firebasestorage.app",
+  messagingSenderId: "724949907964",
+  appId: "1:724949907964:web:f5b76c04de753903d55a79"
 });
 
 const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage(function(payload) {
+  console.log("📩 Background message:", payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon192.png"
+  });
+});
